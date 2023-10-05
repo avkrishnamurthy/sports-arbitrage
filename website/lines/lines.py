892adc593923dfa30f3d69ca54ab53fac3eaae9f
Ajay Krishnamurthy <ajayvkrishnamurthy@gmail.com>
2023-10-05 13:33:34 -0400

    built out populating odds, bookmakers tables, and querying odds table from frontend

--- website/lines/lines.py
+++ website/lines/lines.py
@@ -6,13 +6,11 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
-from website.models import Games
+from website.models import Games, Bookmakers, Odds
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy import func
 from sqlalchemy import or_
-
-
-
+from sqlalchemy.orm.exc import NoResultFound
 
 lines_ = Blueprint('lines', __name__, template_folder='templates', static_url_path='lines/', static_folder='static')
 
@@ -27,17 +25,15 @@ BOOKMAKERS = "fanduel,draftkings,betmgm,foxbet,barstool,pointsbetus,circasports,
 SPORTS = [
     "basketball_nba",
     "americanfootball_nfl",
-    "icehockey_nhl",
     "americanfootball_ncaaf",
     "baseball_mlb",
 ]
-
-# VARIABLES
-SPORT = "americanfootball_nfl"
 DAYS_FROM = "3"
 
+
+
 # API CALL
-def get_api_data():
+def get_games_api_data():
     data_list = []
     for sport in SPORTS:
         request_url = "https://api.the-odds-api.com/v4/sports/{sport}/scores/?apiKey={api_key}&daysFrom={days_from}".format(
@@ -65,7 +61,7 @@ def extract_scores(item):
 
 
 def insert_scores():
-    data_list = get_api_data()
+    data_list = get_games_api_data()
     if not data_list:
         return
     for data in data_list:
@@ -134,8 +130,8 @@ def scores():
 
 @lines_.route('/insert_scores', methods=['POST'])
 def call_insert_scores():
-    insert_scores()  # Assuming insert_scores is defined elsewhere in your app
-    return redirect(url_for('lines.search_games'))  # Redirect back to the search games page after inserting scores
+    insert_scores() 
+    return redirect(url_for('lines.search_games'))
 
 @lines_.route('/search_games', methods=['GET', 'POST'])
 def search_games():
@@ -143,7 +139,6 @@ def search_games():
     if request.method == 'POST':
         query = db.session.query(Games)
         
-        # Filtering based on user input
         date_query = request.form.get('date')
         if date_query:
             date_object = datetime.strptime(date_query, '%Y-%m-%d').date()
@@ -161,19 +156,125 @@ def search_games():
     
     return render_template('games.html', games=games, user=current_user)
 
-
-
-@lines_.route('/lines/odds')
-@login_required
-def upcoming_odds():
-
-    request_url = "https://api.the-odds-api.com/v4/sports/{sport}/odds/?regions={regions}&oddsFormat={odds_format}&markets={markets}&apiKey={api_key}&commenceTimeFrom=".format(
-        sport="upcoming",
+#TODO: make it match current date – needs to be in sync with games fetch
+ODDS_COMMENCE_TIME_FROM = "2023-10-01T00:00:00Z"
+def get_odds_api_data():
+    data_list = []
+    for sport in SPORTS:
+        request_url = "https://api.the-odds-api.com/v4/sports/{sport}/odds?apiKey={api_key}&regions={regions}&markets={markets}&dateFormat=iso&oddsFormat={odds_format}&commenceTimeFrom={commence_time_from}".format(
+        sport=sport,
         api_key=API_KEY,
         markets=MARKETS,
         regions=REGIONS,
         odds_format=ODDS_FORMAT,
-    )
-    data = requests.get(request_url)
-    return data.json()
+        commence_time_from=ODDS_COMMENCE_TIME_FROM
+        )
+        response = requests.get(request_url)
+        data = response.json()
+        data_list.append(data)
+    return data_list
 
+def insert_odds():
+    data_list = get_odds_api_data() 
+    if not data_list:
+        return
+    for data in data_list:
+        if not data:
+            continue
+        for item in data:
+            if not item: continue
+            # Check if the game_id exists in the Games table
+            print(item)
+            game_id = item['id']
+            try:
+                game = db.session.query(Games).filter_by(id=game_id).one()
+            except NoResultFound:
+                continue  # Skip inserting odds if game_id doesn't exist in Games
+
+            for bookmaker in item['bookmakers']:
+                # Get or create the bookmaker
+                bookmaker_obj, _ = get_or_create(db.session, Bookmakers, key=bookmaker['key'], defaults={'title': bookmaker['title']})
+                
+                for market in bookmaker['markets']:
+                    home_team_odds = None
+                    away_team_odds = None
+                    draw_odds = None
+                    if market['key'] == "h2h":
+                        for outcome in market['outcomes']:
+                            outcome_name = outcome['name']
+                            if outcome_name == item['home_team']:
+                                home_team_odds = outcome['price']
+                            elif outcome_name == item['away_team']:
+                                away_team_odds = outcome['price']
+                            elif outcome_name == 'Draw':
+                                draw_odds = outcome['price']
+                            
+                        stmt = insert(Odds).values(
+                            game_id=game.id,
+                            bookmaker_id=bookmaker_obj.id,
+                            home_team_odds = home_team_odds,
+                            away_team_odds = away_team_odds,
+                            sport_key=item['sport_key'],
+                            sport_title=item['sport_title'],
+                            draw_odds = draw_odds,
+                            last_update=market['last_update']
+                        )
+
+                        update_dict = {
+                            Odds.home_team_odds: home_team_odds,
+                            Odds.away_team_odds: away_team_odds,
+                            Odds.draw_odds: draw_odds,
+                            Odds.last_update: market['last_update']
+                        }
+
+                        stmt = stmt.on_conflict_do_update(
+                            index_elements=[Odds.game_id, Odds.bookmaker_id],
+                            set_=update_dict,
+                            where=(Odds.last_update.is_(None) | (Odds.last_update < market['last_update']))
+                        )
+
+                        db.session.execute(stmt)
+
+    db.session.commit()
+
+def get_or_create(session, model, defaults=None, **kwargs):
+    """
+    Gets an object or creates and returns the object if not exists
+    """
+    instance = session.query(model).filter_by(**kwargs).one_or_none()
+    if instance:
+        return instance, False
+    else:
+        params = {**kwargs, **(defaults or {})}
+        instance = model(**params)
+        session.add(instance)
+        session.commit()
+        return instance, True
+
+@lines_.route('/insert_odds', methods=['POST'])
+def call_insert_odds():
+    insert_odds() 
+    return redirect(url_for('lines.search_odds'))
+
+
+@lines_.route('/lines/odds', methods=['GET', 'POST'])
+def odds():
+    odds = []
+    if request.method == 'POST':
+        query = db.session.query(Odds).join(Games).join(Bookmakers)
+        date_query = request.form.get('date')
+        if date_query:
+            date_object = datetime.strptime(date_query, '%Y-%m-%d').date()
+            query = query.filter(func.DATE(Games.commence_time) == date_object)
+            
+        team_query = request.form.get('team')
+        if team_query:
+            query = query.filter((Games.home_team == team_query) | (Games.away_team == team_query))
+            
+        bookmaker_query = request.form.get('bookmaker')
+        if bookmaker_query:
+            query = query.filter(Bookmakers.title == bookmaker_query)
+        
+        odds = query.all()
+
+    return render_template('odds.html', odds=odds, user=current_user)

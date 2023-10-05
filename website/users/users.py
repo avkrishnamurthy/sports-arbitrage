f0f9d21a99ecd0c56bb94f4aa5e3e930f8693d41
dennispark01 <112498972+dennispark01@users.noreply.github.com>
2023-10-05 17:49:50 -0400

    added endpoint for user search and frontend (#6)
    Co-authored-by: dennis_park <dennis.park@duke.edu>

--- website/users/users.py
+++ website/users/users.py
@@ -15,6 +15,20 @@ users_ = Blueprint('users', __name__, template_folder='templates', static_url_pa
 
 load_dotenv()
 
+@users_.route('/check-user-exists', methods=['POST'])
+def check_user_exists():
+    username = request.json.get('username')
+    user = Person.query.filter_by(username=username).first()
+    user_exists = True if user else False
+    return jsonify({'exists': user_exists})
 
 
+@users_.route('/users/<username>', methods=['GET', 'POST'])
+@login_required
+def profile(username):
+    user = Person.query.filter_by(username=username).first()
+    if not user: 
+        flash("User does not exist", category="error")
+        return redirect(url_for("home.home", _external=True))
 
+    return render_template('profile.html', current_user = current_user, user=user)
